26084b641d25650bc2b4d64c6c966fc2eec100d2
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-01 08:48:39 +0100

    first commit

--- js/details.js
+++ js/details.js
@@ -1,1 +1,27 @@
-// javascript for details.html
+const id = new URLSearchParams(window.location.search).get('id');
+const container = document.querySelector('.details');
+const deleteButton = document.querySelector('.delete');
+
+const renderDetails = async () => {
+  const res = await fetch(`http://localhost:3000/posts/${id}`);
+  const post = await res.json();
+
+  const { title, body } = post;
+
+  const template = `
+    <h1>${title}</h1>
+    <p>${body}</p>
+  `;
+
+  container.innerHTML = template;
+}
+
+deleteButton.addEventListener('click', async (e) => {
+  const res = await fetch(`http://localhost:3000/posts/${id}`, {
+    method: 'DELETE',
+  });
+
+  window.location.replace('/index.html');
+});
+
+window.addEventListener('DOMContentLoaded', () => renderDetails());
--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-// javascript for index.html
+const container = document.querySelector('.blogs');
+const searchForm = document.querySelector('.search');
+
+const renderPosts = async (term) => {
+  let uri = '  http://localhost:3000/posts?_sort=likes&_order=desc';
+
+  if(term) uri+= `&q=${term}`;
+
+  const response = await fetch(uri);
+  const posts = await response.json();
+
+  let template = '';
+  
+  posts.forEach(post => {
+    const { title, likes, id } = post;
+    let { body } = post;
+    body = body.slice(0, 200);
+
+    template += `
+      <div class="post">
+        <h2>${title}</h2>
+        <p><small>${likes} likes</small></p>
+        <p>${body}</p>
+        <a href="/details.html?id=${id}">Read more</a>
+      </div>
+    `;
+  });
+
+  container.innerHTML = template;
+}
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  renderPosts(searchForm.term.value.trim());
+})
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
